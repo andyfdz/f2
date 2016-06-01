f9a8648ebb5b9a8e23d44c777a2e63252ea1844f
andyfdz <andrea.fdez.rojas@gmail.com>
2016-05-31 20:02:48 -0500

    coregir user

--- db/migrate/20160518021958_add_fields_to_user.rb
+++ db/migrate/20160518021958_add_fields_to_user.rb
@@ -3,7 +3,7 @@ class AddFieldsToUser < ActiveRecord::Migration
     add_column :users, :name, :string
     add_column :users, :last_name, :string
     add_column :users, :username, :string
-    add_column :users, :sector_id, :id
+    add_column :users, :sector_id, :integer
     add_column :users, :photo, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160531005339) do
-=======
-ActiveRecord::Schema.define(version: 20160527022035) do
->>>>>>> a43e9a38cc6a513e67232147fa7aac698e77b022
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -60,7 +56,32 @@ ActiveRecord::Schema.define(version: 20160527022035) do
     t.datetime "updated_at", null: false
   end
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "last_name"
+    t.string   "username"
+    t.integer  "sector_id"
+    t.string   "photo"
+    t.float    "latitude"
+    t.float    "longidute"
+    t.string   "address"
+    t.string   "provider"
+    t.string   "uid"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
