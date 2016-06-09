a947bb31073593640e60b1fd27c09ef2f75db87b
Andrea Nacif <andrea.nacif@hotmail.com>
2016-06-08 21:04:26 -0500

    mis matches listo

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,7 +4,7 @@ class MatchesController < ApplicationController
   # GET /matches
   # GET /matches.json
   def index
-    @matches = Match.all.joins(:items)
+    @matches = Match.all
   end
 
   # GET /matches/1
