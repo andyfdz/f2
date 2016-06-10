a7168e3bd49eab435887ff397694951c1dcfe2dc
andyfdz <andrea.fdez.rojas@gmail.com>
2016-06-09 22:12:55 -0500

    errores principales

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
+    redirect_to welcome_error_path and return
     #if Administrador
     @items = Item.all
   #else
